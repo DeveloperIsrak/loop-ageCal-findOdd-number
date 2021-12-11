f801c97292705cc81cc3c009c27a4c60f214fe45
Israk <rifathfreelancer@gmail.com>
2021-12-10 23:47:00 -0800

    upload project file

---
+++ assigment/assigment1.js
@@ -0,0 +1,6 @@
+//Create a loop that will rotate from 1000 to 300
+
+
+for( let i = 5; i > 10; i++ ) {
+    console.log(i);
+}
---
+++ assigment/assigment4.js
@@ -0,0 +1,12 @@
+// Create a loop using “for” that will increment by 3 per step and break the loop when it finds a number divisible by 11
+
+for( let i = 1; i <= 100; i+=3){
+    if( i % 11 == 0 ){
+        console.log(` ${ i } this number. which is divisible by 11 `);
+        break;
+    } else{
+        console.log(i);
+    }
+
+    
+}
